5717df662612b2d18680322cd8aeadfe5dafaa97
João <joaov.b.euwest@gmail.com>
2024-10-11 11:32:36 -0300

    #dispatch

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -2,18 +2,16 @@ import { Game } from '../App'
 import Produto from '../components/Produto'
 
 import * as S from './styles'
-
 type Props = {
   jogos: Game[]
-  adicionarAoCarrinho: (jogo: Game) => void
 }
 
-const Produtos = ({ jogos, adicionarAoCarrinho }: Props) => {
+const Produtos = ({ jogos }: Props) => {
   return (
     <>
       <S.Produtos>
         {jogos.map((game) => (
-          <Produto key={game.id} game={game} aoComprar={adicionarAoCarrinho} />
+          <Produto key={game.id} game={game} />
         ))}
       </S.Produtos>
     </>
